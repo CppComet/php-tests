bdfd4885b52fd10a7324ddb1464fea8010f2d801
Trapenok Victor <levhav@yandex.ru>
2018-02-05 09:55:38 +1000

    move tests from https://github.com/CppComet/comet-server to this repo

---
+++ conf.php
@@ -0,0 +1,11 @@
+<?php
+ 
+$serverConf = [
+    "host" => "app.comet-server.ru",
+    "user" => "15",
+    "password" => "lPXBFPqNg3f661JcegBY0N0dPXqUBdHXqj2cHf04PZgLHxT6z55e20ozojvMRvB8",
+    "api_version" => "CometQL_v1",
+    "webTestsUrl" => "https://comet-server.com/doc/CE-api-tests/", 
+    "port" => 3306,
+    "js_nodes" => ["app.comet-server.ru"]  
+];
